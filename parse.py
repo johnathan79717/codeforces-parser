49da96d36a10aa0dcbfafe9582631e9d1819c95d
Po-Hsiang Hao <johnathan79717@gmail.com>
2014-01-15 22:56:36 +0800

    Create testcases

--- parse.py
+++ parse.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python
-import urllib2
-import sys
+from urllib2 import urlopen
+from sys import argv
 from HTMLParser import HTMLParser
+from subprocess import call
 
 class CodeforcesParser(HTMLParser):
-    def __init__(self):
+    def __init__(self, folder):
         HTMLParser.__init__(self)
-        self.numTests = 0
+        self.num_tests = 0
         self.file = None
         self.copying = False
+        self.folder = folder
     def handle_starttag(self, tag, attrs):
         if tag == 'div':
             if attrs == [('class', 'input')]:
-                self.numTests += 1
-                self.file = open('input%d' % self.numTests , 'w')
+                self.num_tests += 1
+                self.file = open(
+                    '%s/input%d' % (self.folder, self.num_tests), 'w')
             elif attrs == [('class', 'output')]:
-                self.file = open('output%d' % self.numTests , 'w')
+                self.file = open(
+                    '%s/output%d' % (self.folder, self.num_tests), 'w')
         elif tag == 'pre':
             if self.file != None:
                 self.copying = True
@@ -32,17 +36,28 @@ class CodeforcesParser(HTMLParser):
         if self.copying:
             self.file.write(data)
 
-def download(contestID, problemID):
-    url = "http://codeforces.com/contest/%s/problem/%s" % (contestID, problemID)
-    return urllib2.urlopen(url).read()
+def download(contest, problem):
+    url = "http://codeforces.com/contest/%s/problem/%s" % (contest, problem)
+    return urlopen(url).read()
+
+def parse(folder, html):
+    parser = CodeforcesParser(folder)
+    parser.feed(html)
+    with open(folder + 'num_tests', 'w') as num_tests:
+        num_tests.write(str(parser.num_tests))
 
 def main():
-    if(len(sys.argv) < 3):
-        print("USAGE: ./parse.py 379 B")
+    if(len(argv) < 2):
+        print("USAGE: ./parse.py 379")
         return
-    html = download(sys.argv[1], sys.argv[2])
-    parser = CodeforcesParser()
-    parser.feed(html)
+    contest = argv[1]
+    call(['mkdir', contest])
+    call(['cd', contest])
+    for problem in ['A', 'B', 'C', 'D', 'E']:
+        folder = '%s/%s/' % (contest, problem)
+        call(['mkdir', folder])
+        html = download(contest, problem)
+        parse(folder, html)
 
 if __name__ == '__main__':
     main()
