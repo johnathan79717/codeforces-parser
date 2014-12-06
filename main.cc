f8c8ffee36f249e1e7c33e45a7e67ad1f3d6447b
Matthias Kauer <mkauer@mit.edu>
2014-12-06 14:09:13 -0500

    added debugger to main template

--- main.cc
+++ main.cc
@@ -2,6 +2,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#ifdef DEBUG
+     #define debug(args...)            {dbg,args; cerr<<endl;}
+#else
+    #define debug(args...)              // Just strip off all debug tokens
+#endif
+
+struct debugger
+{
+    template<typename T> debugger& operator , (const T& v)
+    {    
+        cerr<<v<<" ";    
+        return *this;    
+    }
+} dbg;
+
 int main() 
 {
 
