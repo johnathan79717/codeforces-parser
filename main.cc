f35673a6a18d368d39f85595cf69c2aed3f9a173
Jonathan Hao <johnathan79717@gmail.com>
2014-12-31 15:16:01 +0800

    add my macros

--- main.cc
+++ main.cc
@@ -15,7 +15,40 @@
 #include <deque>
 #include <stack>
 #include <functional>
+#define CASET int ___T, case_n = 1; scanf("%d ", &___T); while (___T-- > 0)
+#define SZ(X) ((int)(X).size())
+#define LEN(X) strlen(X)
+#define FOR(i,c) for(auto &i: c)
+#define ALL(x) (x).begin(),(x).end()
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define REPL(i,x) for(int i=0;x[i];i++)
+#define PER(i,n) for(int i=(n)-1;i>=0;i--)
+#define PER1(i,a,b) for(int i=(a);i>=(b);i--)
+#define RI(x) scanf("%d",&x)
+#define DRI(x) int x;RI(x)
+#define RII(x,y) scanf("%d%d",&x,&y)
+#define DRII(x,y) int x,y;RII(x,y)
+#define RIII(x,y,z) scanf("%d%d%d",&x,&y,&z)
+#define DRIII(x,y,z) int x,y,z;RIII(x,y,z)
+#define RS(x) scanf("%s",x)
+#define PI(x) printf("%d\n",x)
+#define PIS(x) printf("%d ",x)
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define BG begin()
+#define ED end()
+#define PQ priority_queue
+#define MS0(x) memset(x,0,sizeof(x))
+#define MS1(x) memset(x,-1,sizeof(x))
+#define SEP(x) ((x)?'\n':' ')
+#define V(x) vector<x>
 using namespace std;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+typedef long long LL;
+const int INF = 1000000000;
 
 #ifdef DEBUG
      #define debug(args...)            {dbg,args; cerr<<endl;}
